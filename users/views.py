b51e8318fb9855d3d2327d33ab534f0b262a0eea
Daniel <vorobiov.daniel@gmail.com>
2022-04-04 22:33:07 +0300

    Users

--- users/views.py
+++ users/views.py
@@ -1,8 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework.decorators import action
-from rest_framework.mixins import DestroyModelMixin
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from users.serializers import UserSerializer
