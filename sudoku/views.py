24c819ff0709da60c4a69aba620f9a56bf1db496
Daniel <vorobiov.daniel@gmail.com>
2022-05-02 16:50:14 +0300

    Sudoku and Stats CRUD

--- sudoku/views.py
+++ sudoku/views.py
@@ -1,9 +1,16 @@
+from io import BytesIO
+
+import requests
+from django.core.files.temp import NamedTemporaryFile
 from django.shortcuts import get_object_or_404
+from django.template.loader import get_template
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, UpdateModelMixin, CreateModelMixin, DestroyModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from xhtml2pdf import pisa
 
 from sudoku.models import Sudoku
 from sudoku.serializers import SudokuSerializer
@@ -12,6 +19,7 @@ from sudoku.serializers import SudokuSerializer
 class SudokuViewSet(GenericViewSet, CreateModelMixin, UpdateModelMixin, ListModelMixin, DestroyModelMixin):
     queryset = Sudoku.objects.all()
     serializer_class = SudokuSerializer
+    permission_classes = [AllowAny,]
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -32,3 +40,21 @@ class SudokuViewSet(GenericViewSet, CreateModelMixin, UpdateModelMixin, ListMode
     @action(detail=False, methods=['GET'], url_path='own')
     def own(self, request, *args, **kwargs):
         return super().list(request)
+
+    @action(detail=False, methods=['POST'], url_path='pdf')
+    def pdf(self, request, *args, **kwargs):
+        template = get_template('template.html')
+        html = template.render({'context': 'context'})
+        result = BytesIO()
+        pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+
+        file = NamedTemporaryFile(suffix='.pdf')
+        filename = file.name
+        file.write(result.getvalue())
+        file.seek(0)
+
+        files = {'file': open(filename, 'rb')}
+        response = requests.post(FILESERVICE, files=files)
+        pdf_url = response.json().get('url')
+        print(pdf_url)
+        return Response(pdf_url)
